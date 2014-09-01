6d6dd5455e58d2fbdca427d0c86c35d5db7f3249
Shirou WAKAYAMA <shirou.faw@gmail.com>
2014-09-01 22:34:38 +0900

    implement pub and sub.

--- main.go
+++ main.go
@@ -1,57 +1,53 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
 	"os"
 
-	"github.com/Sirupsen/logrus"
+	MQTT "git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.golang.git"
+	log "github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
 )
 
-var log = logrus.New()
 var usage = `
 Usage here
 `
 
 func initFunc() {
-	log.Formatter = new(logrus.TextFormatter)
-	log.Level = logrus.Debug
+	log.SetLevel(log.WarnLevel)
 }
 
 // connects MQTT broker
-func connect(c *cli.Context) (*MQTTClient, error){
-	host := c.String("host")
-	port := c.Int("p")
-	clientId := c.String("i")
+func connect(c *cli.Context, opts *MQTT.ClientOptions) (*MQTTClient, error) {
+	log.Info("Connecting...")
 
-	cafile := c.String("cafile")
-	scheme := "tcp"
-	if cafile != "" {
-		scheme = "ssl"
-	}
-
-	user := c.String("u")
-	password := c.String("P")
-
-	brokerUri := fmt.Sprintf("%s://%s:%d", scheme, host, port)
-	log.Info("Broker URI: %s", brokerUri)
-
-	client := NewMQTTClient()
-
-	log.Debug("Connecting...")
-	_, err := client.Connect(brokerUri, clientId, user, password)
-	if err != nil{
+	client := &MQTTClient{Opts: opts}
+	_, err := client.Connect()
+	if err != nil {
 		return nil, err
 	}
-	log.Debug("Connected")
+	log.Info("Connected")
 
 	return client, nil
 }
 
-
 func publish(c *cli.Context) {
-	client, err := connect(c)
-	if err != nil{
+	if c.Bool("d") {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	opts := NewOption(c)
+
+	willPayload := c.String("will-payload")
+	willQoS := c.Int("will-qos")
+	willRetain := c.Bool("will-retain")
+	willTopic := c.String("will-topic")
+	if willPayload != "" && willTopic != "" {
+		opts.SetWill(willTopic, willPayload, MQTT.QoS(willQoS), willRetain)
+	}
+
+	client, err := connect(c, opts)
+	if err != nil {
 		log.Error(err)
 		os.Exit(1)
 	}
@@ -64,13 +60,51 @@ func publish(c *cli.Context) {
 	}
 	log.Infof("Topic: %s", topic)
 
-	payload := c.String("m")
 	retain := c.Bool("r")
-	log.Infof("Retain: %t", retain)
 
-	client.Publish(topic, []byte(payload), qos, retain)
+	if c.Bool("s") {
+		// Read from Stdin
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			err = client.Publish(topic, []byte(scanner.Text()), qos, retain)
+			if err != nil {
+				log.Error(err)
+			}
 
-	log.Debug("Published")
+		}
+	} else {
+		payload := c.String("m")
+		err = client.Publish(topic, []byte(payload), qos, retain)
+		if err != nil {
+			log.Error(err)
+		}
+
+	}
+	log.Info("Published")
+}
+func subscribe(c *cli.Context) {
+	if c.Bool("d") {
+		log.SetLevel(log.DebugLevel)
+	}
+	opts := NewOption(c)
+	client, err := connect(c, opts)
+	if err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
+
+	qos := c.Int("q")
+	topic := c.String("t")
+	if topic == "" {
+		log.Errorf("Please specify topic")
+		os.Exit(1)
+	}
+	log.Infof("Topic: %s", topic)
+
+	err = client.Subscribe(topic, qos)
+	if err != nil {
+		log.Error(err)
+	}
 }
 
 func main() {
@@ -79,23 +113,74 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "mqttcli"
 	app.Usage = usage
+
+	commonFlags := []cli.Flag{
+		cli.StringFlag{
+			Name:   "host",
+			Value:  "localhost",
+			Usage:  "mqtt host to connect to. Defaults to localhost",
+			EnvVar: "MQTT_HOST"},
+		cli.IntFlag{
+			Name:   "p, port",
+			Value:  1883,
+			Usage:  "network port to connect to. Defaults to 1883",
+			EnvVar: "MQTT_PORT"},
+		cli.StringFlag{
+			Name:   "u,user",
+			Value:  "",
+			Usage:  "provide a username",
+			EnvVar: "MQTT_USERNAME"},
+		cli.StringFlag{
+			Name:   "P,password",
+			Value:  "",
+			Usage:  "provide a password",
+			EnvVar: "MQTT_PASSWORD"},
+		cli.StringFlag{"t", "", "mqtt topic to publish to.", ""},
+		cli.IntFlag{"q", 0, "QoS", ""},
+		cli.StringFlag{"cafile", "", "CA file", ""},
+		cli.StringFlag{"i", "", "ClientiId. Defaults random.", ""},
+		cli.StringFlag{"m", "test message", "Message body", ""},
+		cli.BoolFlag{"r", "message should be retained.", ""},
+		cli.BoolFlag{"d", "enable debug messages", ""},
+		cli.BoolFlag{"insecure", "do not check that the server certificate", ""},
+		//		cli.StringFlag{"conf", "~/.mqtt.cfg", "config file", ""},
+	}
+	pubFlags := append(commonFlags,
+		cli.BoolFlag{"s", "read message from stdin, sending line by line as a message", ""},
+		cli.StringFlag{
+			Name:  "will-payload",
+			Value: "",
+			Usage: "payload for the client Will",
+		},
+		cli.IntFlag{
+			Name:  "will-qos",
+			Value: 0,
+			Usage: "QoS level for the client Will",
+		},
+		cli.BoolFlag{
+			Name:  "will-retain",
+			Usage: "if given, make the client Will retained",
+		},
+		cli.StringFlag{
+			Name:  "will-topic",
+			Value: "",
+			Usage: "the topic on which to publish the client Will",
+		},
+	)
+	subFlags := commonFlags
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "publish",
-			Usage: "publish",
-			Flags: []cli.Flag{
-				cli.StringFlag{"host", "test.mosquitto.org", "Broker IP address"},
-				cli.IntFlag{"p", 1883, "Broker Port"},
-				cli.StringFlag{"t", "", "Topic"},
-				cli.IntFlag{"q", 0, "QoS"},
-				cli.StringFlag{"cafile", "", "CA file"},
-				cli.StringFlag{"u", "", "username"},
-				cli.StringFlag{"P", "", "password"},
-				cli.StringFlag{"i", "client0", "ClientiId"},
-				cli.StringFlag{"m", "test message", "Message body"},
-				cli.BoolFlag{"r", "Retain flag"},
-			},
+			Name:   "pub",
+			Usage:  "publish",
+			Flags:  pubFlags,
 			Action: publish,
+		},
+		{
+			Name:   "sub",
+			Usage:  "subscribe",
+			Flags:  subFlags,
+			Action: subscribe,
 		},
 	}
 	app.Run(os.Args)
--- mqtt.go
+++ mqtt.go
@@ -1,42 +1,25 @@
 package main
 
 import (
-	MQTT "git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.golang.git"
+	"crypto/rand"
 	"crypto/tls"
+	"fmt"
+	"time"
+
+	MQTT "git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.golang.git"
+	log "github.com/Sirupsen/logrus"
+	"github.com/codegangsta/cli"
 )
 
+var MaxClientIdLen = 8
+
 type MQTTClient struct {
-	Opts   *MQTT.ClientOptions
 	Client *MQTT.MqttClient
+	Opts   *MQTT.ClientOptions
 }
 
-func NewMQTTClient() *MQTTClient {
-	return &MQTTClient{}
-}
-
-// connect to MQTT broker
-func (m *MQTTClient) Connect(brokerUri string, clientId string,
-	user string, password string) (*MQTT.MqttClient, error) {
-
-	m.Opts = MQTT.NewClientOptions()
-
-	m.Opts.SetBroker(brokerUri)
-	m.Opts.SetClientId(clientId)
-	m.Opts.SetTraceLevel(MQTT.Critical)
-//	m.Opts.SetTraceLevel(MQTT.Verbose)
-	if user != "" {
-		m.Opts.SetUsername(user)
-	}
-	if password != "" {
-		m.Opts.SetPassword(password)
-	}
-
-	insecure := true
-	if insecure {
-		tlsConfig := &tls.Config{InsecureSkipVerify: true,}
-		m.Opts.SetTlsConfig(tlsConfig)
-	}
-
+// Connects connect to the MQTT broker with Options.
+func (m *MQTTClient) Connect() (*MQTT.MqttClient, error) {
 	m.Client = MQTT.NewClient(m.Opts)
 	_, err := m.Client.Start()
 	if err != nil {
@@ -44,6 +27,7 @@ func (m *MQTTClient) Connect(brokerUri string, clientId string,
 	}
 	return m.Client, nil
 }
+
 func (m *MQTTClient) Publish(topic string, payload []byte, qos int, retain bool) error {
 	mqttmsg := MQTT.NewMessage(payload)
 	// FIXME: validate qos number
@@ -55,4 +39,73 @@ func (m *MQTTClient) Publish(topic string, payload []byte, qos int, retain bool)
 	<-receipt
 
 	return nil
+}
+
+func onMessageReceived(client *MQTT.MqttClient, message MQTT.Message) {
+	fmt.Print(string(message.Payload()))
+}
+func (m *MQTTClient) Subscribe(topic string, qos int) error {
+	topicFilter, err := MQTT.NewTopicFilter(topic, byte(qos))
+	if err != nil {
+		return err
+	}
+	_, err = m.Client.StartSubscription(onMessageReceived, topicFilter)
+	if err != nil {
+		return err
+	}
+
+	for {
+		time.Sleep(1 * time.Second)
+	}
+	return nil
+}
+
+// getRandomClientId returns randomized ClientId.
+func getRandomClientId() string {
+	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, MaxClientIdLen)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
+	}
+	return "mqttcli-" + string(bytes)
+}
+
+// NewOption returns ClientOptions via parsing command line options.
+func NewOption(c *cli.Context) *MQTT.ClientOptions {
+	opts := MQTT.NewClientOptions()
+
+	clientId := c.String("i")
+	if clientId == "" {
+		clientId = getRandomClientId()
+	}
+	opts.SetClientId(clientId)
+	user := c.String("u")
+	if user != "" {
+		opts.SetUsername(user)
+	}
+	password := c.String("P")
+	if password != "" {
+		opts.SetPassword(password)
+	}
+
+	cafile := c.String("cafile")
+	scheme := "tcp"
+	if cafile != "" {
+		scheme = "ssl"
+	}
+	insecure := true
+	if insecure {
+		tlsConfig := &tls.Config{InsecureSkipVerify: true}
+		opts.SetTlsConfig(tlsConfig)
+	}
+
+	host := c.String("host")
+	port := c.Int("p")
+
+	brokerUri := fmt.Sprintf("%s://%s:%d", scheme, host, port)
+	log.Info("Broker URI: %s", brokerUri)
+
+	opts.AddBroker(brokerUri)
+	return opts
 }
